e071f573c668ca64e707e4cb2e507637f45809db
Matheus-Mota16 <matheusdesousamota1604@gmail.com>
2026-05-22 20:38:39 -0300

    sla

--- Mário Clone MIS/Assets/Scripts/playerMovement.cs
+++ Mário Clone MIS/Assets/Scripts/playerMovement.cs
@@ -9,13 +9,20 @@ public class playerMovement : MonoBehaviour
     public Animator anim;
     public float speed;
     public float jumpForce;
+    [SerializeField] bool isJump;
+    [SerializeField] bool inFloor = true;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.1f;
 
     private Vector2 direction;
 
     void Update()
     {
         
-        direction = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rig.linearVelocity.y);
+        // preserve current vertical velocity when setting horizontal
+        float vy = rig != null ? rig.linearVelocity.y : 0f;
+        direction = new Vector2(Input.GetAxisRaw("Horizontal") * speed, vy);
 
         if(Input.GetAxisRaw("Horizontal") < 0)
         {
@@ -27,17 +34,24 @@ public class playerMovement : MonoBehaviour
             sprite.flipX = false;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        // update ground check
+        if (groundCheck != null)
+            inFloor = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer) != null;
+
+        // jump only if grounded
+        if (Input.GetButtonDown("Jump") && inFloor)
         {
-            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if (rig != null)
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
     private void FixedUpdate()
     {
+        if (rig != null)
             rig.linearVelocity = direction;
 
-        if(direction.sqrMagnitude > 0)
+        if (direction.sqrMagnitude > 0)
         {
             anim.Play("playerRun");
         }
@@ -45,6 +59,12 @@ public class playerMovement : MonoBehaviour
         {
             anim.Play("playerIdle");
         }
+    }
+
+    private void Awake()
+    {
+        if (rig == null)
+            rig = GetComponent<Rigidbody2D>();
     }
 
    
